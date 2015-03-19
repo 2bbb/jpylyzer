7544d8bf9831606f5181f90806f4d7628fa90c71
bitsgalore <johan.vanderknijff@kb.nl>
2015-03-19 15:34:13 +0100

    test

--- vagrant/precise32/buildjpylyzerTest.sh
+++ vagrant/precise32/buildjpylyzerTest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## Fetch and build jpylyzer
+git clone --branch recnstna https://github.com/openpreserve/jpylyzer.git
 git clone https://github.com/openpreserve/jpylyzer.git
 cd jpylyzer
 dpkg-buildpackage -tc
